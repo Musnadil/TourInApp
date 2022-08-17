26dc1f4a6e68b797211fdbe5db54e02f1f2ef156
Musnadil <firdaus.musnadil@gmail.com>
2022-08-18 02:48:13 +0700

    fullscreen and theming app

--- app/src/main/java/com/indexdev/tourin/MainActivity.kt
+++ app/src/main/java/com/indexdev/tourin/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.indexdev.tourin
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import com.indexdev.tourin.databinding.ActivityMainBinding
+import com.indexdev.tourin.ui.lightStatusBar
+import com.indexdev.tourin.ui.setFullScreen
 
+@Suppress("DEPRECATION")
+@SuppressLint("InlinedApi")
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val listDestination = listOf(
+        R.id.splashScreenFragment,
+        R.id.firstOnBoardingFragment,
+        R.id.secondOnBoardingFragment,
+        R.id.thirdOnBoardingFragment
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navController = findNavController(R.id.fragment_container)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id in listDestination) {
+                hideSystemUI()
+            } else {
+                showSystemUI()
+            }
+        }
+
+        setFullScreen(window)
+        lightStatusBar(window)
+    }
+
+    private fun hideSystemUI() {
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+    }
+
+    private fun showSystemUI() {
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR or
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
     }
 }
