1e69c444a386f346493548890110c30c1473ac9c
Musnadil <firdaus.musnadil@gmail.com>
2022-08-19 13:50:23 +0700

    temporary home

--- app/src/main/java/com/indexdev/tourin/ui/home/HomeFragment.kt
+++ app/src/main/java/com/indexdev/tourin/ui/home/HomeFragment.kt
@@ -6,14 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.indexdev.tourin.R
+import com.indexdev.tourin.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
-
-
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get()= _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(layoutInflater,container,false)
+        return binding.root
     }
 }
