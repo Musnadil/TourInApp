4e0c15f6868208447cc965242ac5ec69095658f6
Musnadil <firdaus.musnadil@gmail.com>
2022-09-21 14:39:05 +0700

    add permission fine location and ask turn gps on

--- app/src/main/java/com/indexdev/tourin/ui/home/HomeFragment.kt
+++ app/src/main/java/com/indexdev/tourin/ui/home/HomeFragment.kt
@@ -1,12 +1,16 @@
 package com.indexdev.tourin.ui.home
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.android.gms.location.FusedLocationProviderClient
 import com.indexdev.tourin.R
 import com.indexdev.tourin.databinding.FragmentHomeBinding
 import java.util.*
@@ -14,6 +18,11 @@ import java.util.*
 class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+
+    companion object {
+        private const val LOCATION_REQUEST_CODE = 1
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -28,6 +37,18 @@ class HomeFragment : Fragment() {
             findNavController().navigate(R.id.action_homeFragment_to_mapsFragment)
         }
         greeting()
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                requireActivity(), arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                LOCATION_REQUEST_CODE
+            )
+            return
+
+        }
     }
     @SuppressLint("SetTextI18n")
     private fun greeting(){
@@ -41,5 +62,8 @@ class HomeFragment : Fragment() {
             in 21..23 -> {binding.tvGreating.text = "Good Night,"}
             else -> {binding.tvGreating.text = "Good Morning,"}
         }
+    }
+
+    private fun locationPermission(){
     }
 }
--- app/src/main/java/com/indexdev/tourin/ui/maps/MapsFragment.kt
+++ app/src/main/java/com/indexdev/tourin/ui/maps/MapsFragment.kt
@@ -1,12 +1,16 @@
 package com.indexdev.tourin.ui.maps
 
+import android.content.IntentSender
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.RelativeLayout
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -14,6 +18,7 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.tasks.Task
 import com.indexdev.tourin.R
 import com.indexdev.tourin.databinding.FragmentMapsBinding
 
@@ -22,6 +27,7 @@ class MapsFragment : Fragment(), GoogleMap.OnMarkerClickListener {
 
     private var _binding: FragmentMapsBinding? = null
     private val binding get() = _binding!!
+
 
     private val callback = OnMapReadyCallback { googleMap ->
         val sydney = LatLng(-34.0, 151.0)
@@ -71,6 +77,39 @@ class MapsFragment : Fragment(), GoogleMap.OnMarkerClickListener {
         binding.btnBack.setOnClickListener {
             findNavController().popBackStack()
         }
+    }
+    private fun requestDevicesLocationSettings() {
+        val locationReq = LocationRequest.create().apply {
+            interval = 1000
+            fastestInterval = 5000
+            priority = Priority.PRIORITY_HIGH_ACCURACY
+        }
+        val builder = LocationSettingsRequest.Builder()
+            .addLocationRequest(locationReq)
+        val client: SettingsClient = LocationServices.getSettingsClient(requireActivity())
+        val task: Task<LocationSettingsResponse> = client.checkLocationSettings(builder.build())
+        task.addOnSuccessListener { locationSettingsResponse ->
+            //move camera to destination
+            Toast.makeText(requireContext(), "sure", Toast.LENGTH_SHORT).show()
+        }
+        task.addOnFailureListener { exception ->
+            if (exception is ResolvableApiException) {
+                try {
+                    exception.startResolutionForResult(
+                        requireActivity(),
+                        100
+                    )
+                } catch (sendEx: IntentSender.SendIntentException) {
+
+                }
+            }
+        }
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        requestDevicesLocationSettings()
     }
 
     override fun onMarkerClick(p0: Marker) = false
