9f63f6a1139821a9c2b988a0c11979ebda6f63fe
Musnadil <firdaus.musnadil@gmail.com>
2022-10-20 14:38:09 +0700

    Make POJO POI

---
+++ app/src/main/java/com/indexdev/tourin/data/model/response/ResponsePOI.kt
@@ -0,0 +1,19 @@
+package com.indexdev.tourin.data.model.response
+
+
+import com.google.gson.annotations.SerializedName
+
+data class ResponsePOI(
+    @SerializedName("id_poi")
+    val idPoi: String,
+    @SerializedName("kode_fasilitas")
+    val kodeFasilitas: String,
+    @SerializedName("kode_wisata")
+    val kodeWisata: String,
+    @SerializedName("lat")
+    val lat: String,
+    @SerializedName("longi")
+    val longi: String,
+    @SerializedName("nama_fasilitas")
+    val namaFasilitas: String
+)
