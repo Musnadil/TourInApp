09f1ef89c67a0c5b38f72c6772aca96a26dad2d5
Musnadil <firdaus.musnadil@gmail.com>
2023-05-15 16:44:55 +0700

    ProductPartnerFragment hit api profile partner by id

--- app/src/main/java/com/indexdev/tourin/data/api/ApiHelper.kt
+++ app/src/main/java/com/indexdev/tourin/data/api/ApiHelper.kt
@@ -4,7 +4,9 @@ import com.indexdev.tourin.data.model.request.LoginRequest
 import com.indexdev.tourin.data.model.request.RateRequest
 import com.indexdev.tourin.data.model.request.RegisterRequest
 import com.indexdev.tourin.data.model.request.UpdateUserRequest
+import com.indexdev.tourin.data.model.response.ResponseUserMitraById
 import okhttp3.RequestBody
+import retrofit2.http.Path
 
 class ApiHelper(private val apiService: ApiService) {
     suspend fun authRegister(registerRequest: RegisterRequest) = apiService.authRegister(registerRequest)
@@ -15,5 +17,7 @@ class ApiHelper(private val apiService: ApiService) {
     suspend fun editUsername(id:Int, updateUserRequest: UpdateUserRequest) = apiService.editUsername(id, updateUserRequest)
     suspend fun getRecommendationList() = apiService.getRecommendationList()
     suspend fun getAllUserMitra() = apiService.getAllUserMitra()
+    suspend fun getUserPartnerById(id: Int) = apiService.getUserPartnerById(id)
+
 
 }
